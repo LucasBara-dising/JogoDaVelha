1764041e021b6366409e9cd5df2e03cbdb924bf7
Lucas Barauna <82176900+LucasBara-dising@users.noreply.github.com>
2023-06-03 16:23:54 -0300

    implementação do modo contra maquina no jogo

--- app/src/main/java/com/example/jogodavelhakotlin/MainActivity.kt
+++ app/src/main/java/com/example/jogodavelhakotlin/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.jogodavelhakotlin
 
-import android.annotation.SuppressLint
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -12,24 +12,55 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnReinicia.setOnClickListener{
+        btnReinicia.setOnClickListener {
             reinicairJogo()
         }
+
+        //-----------------------dialog-----------------
+        val dialogBinding=layoutInflater.inflate(R.layout.style_dialog_begin_game, null)
+        val dialog=Dialog(this)
+
+        dialog.setContentView(dialogBinding)
+        dialog.setCancelable(false)
+        dialog.window?.setBackgroundDrawableResource(R.drawable.round_corner)
+        dialog.show()
+
+        val btn2Players= dialogBinding.findViewById<Button>(R.id.btn_gamePlayer)
+        btn2Players.setOnClickListener{
+            modoGame="2jogadores"
+            dialog.dismiss()
+        }
+
+        val btnGameIA= dialogBinding.findViewById<Button>(R.id.btn_gameIa)
+        btnGameIA.setOnClickListener{
+            modoGame="maquina"
+            dialog.dismiss()
+        }
+
     }
 
-    fun btnClick(view: View){
-        //val= valor fixo
-        //var= valor vriavel
 
+    //arrays para guardar as jogadas
+    var player1=ArrayList<String>()
+    var player2=ArrayList<String>()
+
+    //modo de jogo
+    var modoGame=""
+
+    //turnos
+    var activePlayer=1
+
+    fun btnClick(view: View){
         val btnSelect= view as ImageView
         var boxId=""
 
         when(btnSelect.id){
-            //atrelanod um valor a cada box
+            //atrelando um valor a cada box
             R.id.btn_A1->boxId="A1"
             R.id.btn_A2->boxId="A2"
             R.id.btn_A3->boxId="A3"
@@ -41,39 +72,156 @@ class MainActivity : AppCompatActivity() {
             R.id.btn_C3->boxId="C3"
         }
 
-        playGame(boxId,btnSelect)
+        //2 jogadores
+        if(modoGame == "2jogadores"){
+            playGame2Players(boxId,btnSelect)
+        }
+        //maquina
+        else if(modoGame.equals("maquina")){
+            //se for vez da maquina recebe tudo null
+            if(activePlayer==2){
+                playGameIA(null,null)
+            } else{
+                //se for player recebe valores
+                playGameIA(boxId,btnSelect)
+            }
+
+        }
     }
 
-    //arrays para guardar as jogadas
-    var player1=ArrayList<String>()
-    var player2=ArrayList<String>()
-
-    //turnos
-    var activePlayer=1
-
-    @SuppressLint("SetTextI18n")
-    fun playGame(boxId:String, btnSelect:ImageView){
+    private fun playGame2Players(boxId:String, btnSelect:ImageView){
         if (activePlayer==1){
-            txtvez.text="Vez do Jogador 2"
+            txtvez.text= R.string.txtVezjogador2.toString()
             btnSelect.setImageResource(R.drawable.cross)
             player1.add(boxId)
             activePlayer=2
         }
         else{
-            txtvez.text= "Vez do Jogador 1"
+            txtvez.text= R.string.txtVezjogador1.toString()
             btnSelect.setImageResource(R.drawable.letter_o)
             player2.add(boxId)
             activePlayer=1
         }
 
+        //desabilita o btn ja clicado
         btnSelect.isEnabled=false
 
         //toda vez que tiver uma jogada verifica o ganhador
         checkWiner()
     }
+
+    private fun playGameIA(boxId:String?, btnSelect: ImageView?){
+        //jogador
+        if (activePlayer==1 && boxId!=null && btnSelect!=null){
+            txtvez.text=R.string.txtVezMaquina.toString()
+            btnSelect.setImageResource(R.drawable.letter_o)
+            btnSelect.isEnabled=false
+            player1.add(boxId)
+            activePlayer=2
+            checkWiner()
+        }
+        //máquina
+        if (activePlayer==2){
+            txtvez.text=R.string.txtSuaVez.toString()
+            //retorna uma box pro jogo
+            maquinaJoga()
+            activePlayer=1
+            checkWiner()
+        }
+
+    }
+
+    //checka se a caixa ja foi usada
+    private fun checkClick(boxId: String):Boolean{
+        return player1.contains(boxId)==false && player2.contains(boxId)==false
+    }
+
+    fun selectBoxCross(boxId: String) {
+        player2.add(boxId)
+
+        val imgCross=when(boxId){
+            "A1"->findViewById(R.id.btn_A1)
+            "A2"->findViewById(R.id.btn_A2)
+            "A3"->findViewById(R.id.btn_A3)
+
+            "B1"->findViewById(R.id.btn_B1)
+            "B2"->findViewById(R.id.btn_B2)
+            "B3"->findViewById(R.id.btn_B3)
+
+            "C1"->findViewById(R.id.btn_C1)
+            "C2"->findViewById(R.id.btn_C2)
+            "C3"->findViewById(R.id.btn_C3)
+
+            else->findViewById<ImageView>(R.id.btn_A3)
+        }
+
+        imgCross.setImageResource(R.drawable.cross)
+        imgCross.isEnabled=false
+    }
+
+    fun geraJogada():String{
+        val MoviGame = when((1..8).random()) {
+            1 -> "A1"
+            2 -> "A2"
+            3 -> "A3"
+            4 -> "B1"
+            5 -> "B2"
+            6 -> "B3"
+            7 -> "C1"
+            8 -> "C2"
+            else -> "C3"
+        }
+        return MoviGame
+    }
+
+    fun maquinaJoga(){
+        //primneira jogada nos cantos
+        if(player2.isEmpty()){
+            val FirstGame = when((1..3).random()) {
+                1 -> "A1"
+                2 -> "A3"
+                3 -> "C1"
+                else -> "C3"
+            }
+            //verifica se pode, se não poder tenta denovo
+            if (checkClick(FirstGame)==true){
+                selectBoxCross(FirstGame)
+            }
+            else{
+                maquinaJoga()
+            }
+        }
+
+        else  if(player2.size>1) {
+            //gera uma caixa verifica se pode, se não poder tenta denovo
+            val jogadaMqn=geraJogada()
+
+            if (checkClick(jogadaMqn)==true){
+                selectBoxCross(jogadaMqn)
+            }
+            else{
+                maquinaJoga()
+            }
+        }
+        //verifica se pode jogar no maio, se não poder tenta denovo
+        else if(player2.size==1 && checkClick("B2")==true) {
+            selectBoxCross("B2")
+        }
+        else {
+            val jogadaMqn=geraJogada()
+            if (checkClick(jogadaMqn)==true){
+                selectBoxCross(jogadaMqn)
+            }
+            else{
+                maquinaJoga()
+            }
+
+        }
+    }
+
     var contRodadas=0
     //checa quem ganhou
-    fun checkWiner(){
+    private fun checkWiner(){
         var winer=-1
         contRodadas++
 
@@ -117,7 +265,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun vitoria(QuemGanhou:String){
+    private fun vitoria(QuemGanhou:String){
         player1.clear()
         player2.clear()
 
@@ -125,7 +273,7 @@ class MainActivity : AppCompatActivity() {
         val dialog=Dialog(this)
 
         dialog.setContentView(dialogBinding)
-        dialog.setCancelable(true)
+        dialog.setCancelable(false)
         dialog.window?.setBackgroundDrawableResource(R.drawable.round_corner)
         dialog.show()
 
